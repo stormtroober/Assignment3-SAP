fb4e262bb1f284a9bc0b0c0ea32a38c6504fcc80
Alessandro Becci <trofui@gmail.com>
2025-04-19 17:16:40 +0200

    chore: add SLF4J and Logback dependencies for logging support

--- ebikes-kafka/microservices/ride-microservice/app/build.gradle.kts
+++ ebikes-kafka/microservices/ride-microservice/app/build.gradle.kts
@@ -23,7 +23,8 @@ dependencies {
     implementation("io.vertx:vertx-web:4.4.0")
     implementation("io.vertx:vertx-web-client:4.4.0")
     implementation("io.vertx:vertx-config:4.4.0")
-
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
     // Add Micrometer and Prometheus dependencies
     implementation("io.micrometer:micrometer-core:1.12.3")
     implementation("io.micrometer:micrometer-registry-prometheus:1.12.3")

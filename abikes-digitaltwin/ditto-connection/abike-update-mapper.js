92a45c7c7ff411159eeb9932e18b6be67f59d637
Alessandro Becci <trofui@gmail.com>
2025-06-05 16:02:25 +0200

    feat: creation of thing is successfull now

--- abikes-digitaltwin/ditto-connection/abike-update-mapper.js
+++ abikes-digitaltwin/ditto-connection/abike-update-mapper.js
@@ -5,16 +5,36 @@ function mapToDittoProtocolMsg(headers, textPayload, bytePayload, contentType) {
     } catch (e) {
         return null;
     }
-    var thingId = jsonData.id;
+    var thingId = "abike:" + jsonData.id;
     if (!thingId) return null;
 
     var features = {};
-    for (var key in jsonData)
-        if (jsonData.hasOwnProperty(key)) features[key] = {
-            properties: {
-                value: jsonData[key]
-            }
-        };
-    var topic = headers['kafka.topic'] || '';
-    return Ditto.buildDittoProtocolMsg('default', thingId, 'things', 'twin', 'commands', 'modify', '/features', headers, features);
+    jsonData.id = thingId;
+    for (var key in jsonData) {
+        if (jsonData.hasOwnProperty(key) && key !== 'id') {
+            features[key] = {
+                properties: {
+                    value: jsonData[key]
+                }
+            };
+        }
+    }
+
+    var value = {
+        thingId: "abike:" + thingId,
+        policyId: "org.eclipse.ditto:simple-policy",
+        features: features
+    };
+
+    return Ditto.buildDittoProtocolMsg(
+        "org.eclipse.ditto",
+        thingId,
+        "things",
+        "twin",
+        "commands",
+        "modify",
+        "/",
+        headers,
+        value
+    );
 }

e9b0337d12cb26a1141ef77b6d9f2576be426beb
Alessandro Becci <trofui@gmail.com>
2025-06-23 12:44:17 +0200

    refactor: simplify StationMapServiceAPI and implementation by removing unused methods

--- abikes-digitaltwin/microservices/map-microservice/app/src/main/java/application/ports/StationMapServiceAPI.java
+++ abikes-digitaltwin/microservices/map-microservice/app/src/main/java/application/ports/StationMapServiceAPI.java
@@ -1,16 +1,12 @@
 package application.ports;
 
 import domain.model.Station;
-import java.util.List;
+
 import java.util.concurrent.CompletableFuture;
 
 public interface StationMapServiceAPI {
 
   CompletableFuture<Void> updateStation(Station station);
-
-  CompletableFuture<Void> updateStations(List<Station> stations);
-
-  CompletableFuture<Void> deassignBikeFromStation(String bikeId);
 
   void getAllStations();
 }
